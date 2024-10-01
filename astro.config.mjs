87498fd8f0b51a8ab1070bda2dcde4984e44aa16
juancarlossa <juank987610@gmail.com>
2024-10-01 23:56:37 +0200

    Fix bugs 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import svelte from "@astrojs/svelte";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
