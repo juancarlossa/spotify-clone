2da3c047959982da96d1d9fb81df758c0056bf6b
juancarlossa <juank987610@gmail.com>
2024-10-01 23:45:26 +0200

    Version 1.0

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+  let name = "Juan";
+
+  if (currentHour < 12) {
+    greeting = "Buenos dias";
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes";
+  } else {
+    greeting = "Buenas noches";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting + " " + name}</h1>
